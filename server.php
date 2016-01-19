5673a6c00a879bb9cf8d55f7850655951b7af6c3
Martin <marto.132@gmail.com>
2016-01-19 22:56:18 +0200

    upload

---
+++ server.php
@@ -0,0 +1,75 @@
+<?php
+$list = [
+	[
+		'name' => 'Ivan',
+		'pic' => 'man.png',
+		'position' => 'Coder',
+		'bardDay' => '11.03.1991',
+		'sex' => 'male'
+	],
+	[
+		'name' => 'Ivana',
+		'pic' => 'girl.png',
+		'position' => 'Coder',
+		'bardDay' => '25.12.1985',
+		'sex' => 'female'
+	],
+	[
+		'name' => 'Jhon',
+		'pic' => 'man1.png',
+		'position' => 'Boss',
+		'bardDay' => '11.06.1989',
+		'sex' => 'male'
+	],
+	[
+		'name' => 'Ana',
+		'pic' => 'featured-girl.png',
+		'position' => 'Assistant',
+		'bardDay' => '10.07.1990',
+		'sex' => 'female'
+	],
+	[
+		'name' => 'Georgi',
+		'pic' => 'man2.png',
+		'position' => 'Coder',
+		'bardDay' => '11.06.1987',
+		'sex' => 'male'
+	],
+	[
+		'name' => 'Vanesa',
+		'pic' => 'emplotee.png',
+		'position' => 'Assistant',
+		'bardDay' => '11.03.1991',
+		'sex' => 'female'
+	],
+	[
+		'name' => 'Edi',
+		'pic' => 'man3.png',
+		'position' => 'Coder',
+		'bardDay' => '21.10.1981',
+		'sex' => 'male'
+	],
+	[
+		'name' => 'Alan',
+		'pic' => 'PNG6557.png',
+		'position' => 'Developer',
+		'bardDay' => '11.06.1988',
+		'sex' => 'male'
+	],
+	[
+		'name' => 'Mariq',
+		'pic' => 'slider-girl.png',
+		'position' => 'Developer',
+		'bardDay' => '15.05.1984',
+		'sex' => 'female'
+	],
+	[
+		'name' => 'Pesho',
+		'pic' => 'happy-man.png',
+		'position' => 'Administrator',
+		'bardDay' => '11.03.1980',
+		'sex' => 'male'
+	]
+];
+
+echo json_encode($list);
